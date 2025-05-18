12e5f3d97ff1596cf183ad2b3a222b4ad715900a
netkro <kroging@gmail.com>
2025-05-18 22:06:01 +0900

    fix: chagne to ts

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const port = 3000
-
-app.use(cors())
-
+"use strict";
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+app.use(cors());
 app.get("/", function (req, res) {
-  res.send(`
+    res.send(`
     <a href='http://localhost:3000/sound/dog'>dog</a>
     <a href='http://localhost:3000/sound/cat'>cat</a>
-    hello world`)
-})
-
+    hello world`);
+});
 app.get("/sound/:name", function (req, res) {
-  const { name } = req.params
-
-  if (name === "dog") {
-    res.json({ sound: "bowwow" })
-  } else if (name === "cat") {
-    res.json({ sound: "nyaw" })
-  } else if (name === "pig") {
-    res.json({ sound: "buibui" })
-  } else {
-    res.json({ sound: "unkown" })
-  }
-})
-
+    const { name } = req.params;
+    if (name === "dog") {
+        res.json({ sound: "bowwow" });
+    }
+    else if (name === "cat") {
+        res.json({ sound: "nyaw" });
+    }
+    else if (name === "pig") {
+        res.json({ sound: "buibui" });
+    }
+    else {
+        res.json({ sound: "unkown" });
+    }
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-  console.log(`run to http://localhost:${port}/`)
-})
+    console.log(`Example app listening on port ${port}`);
+    console.log(`run to http://localhost:${port}/`);
+});
---
+++ index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express"
+import cors from "cors"
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get("/", function (req: Request, res: Response) {
+  res.send(`
+    <a href='http://localhost:3000/sound/dog'>dog</a>
+    <a href='http://localhost:3000/sound/cat'>cat</a>
+    hello world`)
+})
+
+app.get("/sound/:name", function (req, res) {
+  const { name } = req.params
+
+  if (name === "dog") {
+    res.json({ sound: "bowwow" })
+  } else if (name === "cat") {
+    res.json({ sound: "nyaw" })
+  } else if (name === "pig") {
+    res.json({ sound: "buibui" })
+  } else {
+    res.json({ sound: "unkown" })
+  }
+
+  console.log("sound", name)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+  console.log(`run to http://localhost:${port}/`)
+})
